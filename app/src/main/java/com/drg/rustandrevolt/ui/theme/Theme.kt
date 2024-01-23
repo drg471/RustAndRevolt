f4a0ea2099718ff65cf8222514ed4cf4ec13fae1
David <david@d.com>
2024-01-23 09:58:29 +0100

    Force app view in Light Mode

--- app/src/main/java/com/drg/rustandrevolt/ui/theme/Theme.kt
+++ app/src/main/java/com/drg/rustandrevolt/ui/theme/Theme.kt
@@ -39,7 +39,9 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun RustAndRevoltTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    //darkTheme: Boolean = false (fuerza a la app verse en modo claro)
+    //darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
